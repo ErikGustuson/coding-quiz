acb840b55f5326048b338fe61f7641b9ac558390
Erik Gustuson <gusgus1919@gmail.com>
2021-09-25 01:10:51 -0700

    add starting pseudo code and link css and set var colors

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// TODO: starting screen with button to select start
+// TODO: have listener to watch for button press to start timer and display first question
+// TODO: write at least 10 different question with 4 possible answers ?data strings so that i can update ordered list in html
+// TODO: make sure that correct answers are not in same location on list
+// TODO: have listener on container to look for button press of answer
+// TODO: when correct answers are selected have score added to running total
+// TODO: when incorrect have time subtracted from timer
+// TODO: end game condition leads to score screen when initials can be stored
+// TODO: log initials and score to string and save to local storage so they can return to game
+// TODO: go to high score screen, pull from local storage to display records
+// TODO: have link that can view high score at any time (optional) also button that clears high score (optional)
+// TODO: on high score screen have button to start quiz which will start function repeat quiz
